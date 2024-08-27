27f41e2541bb133ec0b5bcea9edc4ad55b7e5f08
Anna Cristina <cristinaanna05@gmail.com>
2024-08-27 17:45:51 -0300

    Add files via upload

---
+++ Hora de Codar 2 (At-5).kt
@@ -0,0 +1,20 @@
+fun main(){
+    //5.  Faça um programa que leia 6 valores informados pelo usuário.
+    //calcule, exiba os números informados e escreva a média aritmética desses valores lidos.
+
+
+            println("Informe um número:")
+    var n1= readln().toInt()
+    println("Informe um número:")
+    var n2= readln().toInt()
+    println("Informe um número:")
+    var n3= readln().toInt()
+    println("Informe um número:")
+    var n4= readln().toInt()
+    println("Informe um número:")
+    var n5= readln().toInt()
+    println("Informe um número:")
+    var n6= readln().toInt()
+    var media=(n1+n2+n3+n4+n5+n6) /6
+    println(" A media aritmetica dos valores é: $media")
+}
