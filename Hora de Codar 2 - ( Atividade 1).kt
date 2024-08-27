55ce779773f4cea7bf7a5b25a677b8037e4db12f
Anna Cristina <cristinaanna05@gmail.com>
2024-08-27 12:55:24 -0300

    Add files via upload

---
+++ Hora de Codar 2 - ( Atividade 1).kt
@@ -0,0 +1,20 @@
+class `Hoda de Codar 2`
+ fun main() {
+     //1. Escreva um programa em que o usuário informe dois números. Então escreva em tela o maior deles.
+     println(" Informe  o primeiro Número:")
+     var numero1= readln().toDouble()
+     println(" Informe o segundo Número:")
+     var numero2= readln().toDouble()
+
+     if (numero1 > numero2){
+         println("O primeiro número é o maior")}
+
+     else if (numero1 < numero2){
+         println("O segundo número é o maior")}
+
+     else {
+         println(" Os números são iguais")}
+     }
+
+
+
